b175af6ed96d310b3293c7b3b265380780ffecee
saimanohar999 <90305659+saimanohar999@users.noreply.github.com>
2021-09-14 10:54:34 +0530

    Update request.py

--- request.py
+++ request.py
@@ -6,6 +6,8 @@ email_dict = {}
 response = requests.get("https://gorest.co.in/public/v1/comments")
 if response.status_code == 200:
     response = response.json()
+else:
+    print("ERROR")
 for item in response['data']:
     email_dict[item['id']]=item['email']
     email_list.append(item['email'])
